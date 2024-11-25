688235faa5756fffe8d36d215645f3e42c170cbd
ravi-sidharth <thenewravy@gmail.com>
2024-11-25 18:27:15 +0530

    update

--- app.js
+++ app.js
@@ -4,26 +4,26 @@ let hard = "hard"
 let dummyScore = [10, 10, 15, 15, 20, 20]
 let player1Score = 0
 let player2Score = 0
-let count = 0;
-let playedCategories = [];
+let count = 0
+let playedCategories = []
 let currentCategory = ""
 let player1Name = ""
 let player2Name = ""
-let questionBank = []
 let highScore = 0
 let StartGameAgainIntervalID = ""
 let winner = ""
 
-async function getData(category, difficulties) {
+async function getData(category,difficulties) {
     const res = await fetch(`https://the-trivia-api.com/v2/questions?limit=1&categories=${category}&difficulties=${difficulties}`);
-    const data = await res.json();
+    const data = await res.json()
     currentCategory = category
-    playedCategories.push(category);
-    showQuestion(data);
+    playedCategories.push(category)
+    showQuestion(data)
 }
+
 const categorySelector = document.getElementById('category-select');
 const formSubmitBtn = document.querySelector('#player-form')
-formSubmitBtn.addEventListener('submit', (event) => {
+formSubmitBtn.addEventListener('submit',(event) => {
     event.preventDefault();
     player1Name = document.querySelector('#player1').value;
     player2Name = document.querySelector('#player2').value;
@@ -32,13 +32,13 @@ formSubmitBtn.addEventListener('submit', (event) => {
     playersName.innerHTML = `Player 1:${player1Name} Vs Player 2:${player2Name}`
     document.querySelector('#title').appendChild(playersName)
 
-    formSubmitBtn.className = "hidden";
-    categorySelector.className = "block";
+    formSubmitBtn.className="hidden";
+    categorySelector.className="block";
 });
 
-const categorySelect = document.getElementById('categories');
+const categorySelect=document.getElementById('categories');
 categorySelect.addEventListener('change', (event) => {
-    const selectedCategory = event.target.value;
+    const selectedCategory=event.target.value;
     if (StartGameAgainIntervalID) {
         clearInterval(StartGameAgainIntervalID);
     }
@@ -65,20 +65,21 @@ function showQuestion(data) {
     questionText.className="text-2xl font-bold"
     questionText.innerHTML = `Question No ${count + 1}: ${data[0].question.text}`;
     questionContainer.appendChild(questionText);
+    console.log(`Correct Answer:${data[0].correctAnswer}`);
+    
 
     //For question option and suffling
     const options = [...data[0].incorrectAnswers, data[0].correctAnswer].sort(() => Math.random() - 0.5);
     const optionList = document.createElement('ol');
-    let optionNumber =1
+    let optionNumber = 1
     options.forEach(option => {
         const list = document.createElement('li')
         list.innerHTML =` ${optionNumber}- ${option}`;
-        
         optionNumber+=1
         optionList.appendChild(list);
         questionContainer.appendChild(optionList)
 
-        // addEvent listener in option option is right or worng
+        // Checking the option is right or worng
         list.classList.add("hover:bg-blue-400", "mb-2","mt-2")
         list.addEventListener('click',()=> {
             if (count % 2 == 0) {
@@ -86,7 +87,7 @@ function showQuestion(data) {
                     player1Score += dummyScore[count]
                     player.innerText = `Weldone your Answer is correct and now your score is ${player1Score}`;
                     player.classList.add("text-4xl", "font-bold", "text-green-400")
-                    list.classList.add = ("text-2xl", "font-bold", "text-green-400")
+                    list.classList.add("text-2xl", "font-bold", "text-green-400")
 
                 } else {
                     player.innerText =`Your answer is incorrect and correct answer is ${data[0].correctAnswer}`;
@@ -127,19 +128,6 @@ function showQuestion(data) {
 const finalScore = document.createElement('h2');
 function endGame() {
     questionContainer.innerText = ""
-    if (player1Score > player2Score) {
-        highScore = player1Score
-        winner = player1Name
-    } else if (player1Score==player2Score) {
-        winner="No one, Because both score is same.Tied the game!"
-    }
-    else {
-        highScore = player2Score
-        winner = player2Name
-    }
-    finalScore.innerText = `Game Over! Player 1: ${player1Score}, Player 2: ${player2Score}, High Score: ${highScore} and Winner is ${winner}`;
-    questionContainer.appendChild(finalScore);
-
     const gameContinue = document.createElement('button');
     const gameEnd = document.createElement('button');
     gameContinue.innerText = "Play Again";
@@ -149,18 +137,34 @@ function endGame() {
     questionContainer.appendChild(gameContinue);
     questionContainer.appendChild(gameEnd);
 
-    // if user wants to play again then he can play
+    //if user wants to play again then they can play
     gameContinue.addEventListener('click', () => {
-        categorySelector.style.display = "block";
+        categorySelector.style.display="block";
         count = 0;
-        player1Score = 0;
-        player2Score = 0;
         winner = ""
         showAgainQuestion()
     })
     // user wants to end the game 
     gameEnd.addEventListener('click', () => {
-        reload();
+        questionContainer.innerText = ""
+    
+        if (player1Score > player2Score) {
+            highScore = player1Score
+            winner = player1Name
+        } else if (player1Score==player2Score) {
+            winner="No one, Because both score is same.Tied the game!"
+        }
+        else {
+            highScore = player2Score
+            winner = player2Name
+        }
+        finalScore.innerText = `Game Over! Player 1: ${player1Score}, Player 2: ${player2Score}, High Score: ${highScore} and Winner is ${winner}`;
+        questionContainer.appendChild(finalScore);
+
+        setTimeout(()=> {
+            reload()
+        },5000)
+
     })
 }
 
@@ -174,7 +178,7 @@ function showAgainQuestion() {
         }
     }
 
-    if (selectElement.options.length > 1) {
+    if (selectElement.options.length>1){
         questionContainer.innerText = ""
         const h1 = document.createElement('h1')
         h1.innerText = "Select new category which you want to play next within 10 Second"
